a2bee67b5de344bbdb6c023dbbd4d4e2e5864aa7
Laura Leinweber <2478354L@student.gla.ac.uk>
2021-01-21 16:48:42 +0000

    Chapter 9 Register 9.6 completed

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -105,11 +105,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-PASSWORD_HASHERS = ['django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-                    'django.contrib.auth.hashers.BCryptPasswordHasher',
-                    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-                    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-                    ]
+PASSWORD_HASHERS = (
+'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
                     
 
 
